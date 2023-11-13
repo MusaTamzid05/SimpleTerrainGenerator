bc6281298b335576207c981f41386d466927a700
MusaTamzid05 <tamzid101@gmail.com>
2023-11-14 03:33:42 +0600

    Added LinearInterpolator

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "window.h"
+#include "linear_interpolator.h"
+#include <iostream>
 
 
 int main(int argc, char** argv) {
-    Window window;
-    window.run();
+    LinearInterpolator interpolator(0.0f, 50.0f, -1.0f, 1.0f);
+
+    for(int i = 0; i <= 50; i += 1) {
+        std::cout << i << " => " << interpolator.map(i) << "\n";
+
+    }
+    //Window window;
+    //window.run();
     return 0;
 }
